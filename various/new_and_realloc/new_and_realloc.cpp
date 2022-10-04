b05b24a02eab521013c8f562addf49b29e770f17
Pascal MOSBAH <pascal.mosbah@isen.fr>
2022-10-04 14:21:45 +0200

    Up

--- various/new_and_realloc/new_and_realloc.cpp
+++ various/new_and_realloc/new_and_realloc.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 
 using namespace std;
@@ -11,10 +12,6 @@ void my_allocation(T_ *&ptr, const size_t &old_size, const size_t &new_size)
     {
         auto new_ptr = new T_[new_size];
         memcpy(new_ptr, ptr, old_size * sizeof(T_));
-        if (new_ptr != ptr)
-        {
-            cout << "Pointer address has changed at iteration : " << count << endl;
-        }
         delete[] ptr;
         ptr = new_ptr;
     }
@@ -25,11 +22,37 @@ void my_allocation(T_ *&ptr, const size_t &old_size, const size_t &new_size)
     ++count;
 }
 
+template <typename T_>
+void my_allocation_using_C(T_ *&ptr, const size_t &new_size)
+{
+    static unsigned count = 1;
+    if (ptr)
+    {
+        auto old_ptr = ptr;
+        ptr = static_cast<T_*>(realloc(ptr, new_size * sizeof(T_)));
+        if (!ptr)
+        {
+            cout << "Bad allocation using malloc" << endl;
+            exit(EXIT_FAILURE);
+        }
+        if (old_ptr != ptr)
+        {
+            cout << "Pointer address has changed at iteration : " << count << endl;
+        }
+    }
+    else
+    {
+        ptr = static_cast<T_*>(malloc(new_size * sizeof(T_)));
+    }
+    ++count;
+}
+
 int main()
 {
-    const size_t size = 1 << 10;
+    const size_t size = 1 << 30;
     cout << size << endl;
     float *tab_float = nullptr;
+    float *tab_float_using_C = nullptr;
 
     try
     {
@@ -44,5 +67,10 @@ int main()
         cout << "Memory Allocation"
              << e.what()
              << endl;
+    }
+
+    for (auto i = 0; i < 100; ++i)
+    {
+        my_allocation_using_C(tab_float_using_C, size);
     }
 }
