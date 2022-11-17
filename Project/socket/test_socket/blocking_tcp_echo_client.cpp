1deb576ca7f3cb485fa87de3601a89774d42e290
PascalM <pascal.mosbah@kathab.fr>
2022-11-17 19:56:52 +0100

    Up

--- Project/socket/test_socket/blocking_tcp_echo_client.cpp
+++ Project/socket/test_socket/blocking_tcp_echo_client.cpp
@@ -1,7 +1,8 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include <boost/asio.hpp>
+
+#include "socket_tools.hpp"
 
 #include <boost/archive/binary_iarchive.hpp>
 #include <boost/archive/binary_oarchive.hpp>
@@ -10,24 +11,8 @@
 
 using boost::asio::ip::tcp;
 
-class counter_streambuf : public std::streambuf {
-public:
-    using std::streambuf::streambuf;
 
-    size_t size() const { return m_size; }
 
-protected:
-    std::streamsize xsputn(const char_type* __s, std::streamsize __n) override 
-    { this->m_size += __n; return __n; }
-
-private:
-    size_t m_size = 0;
-};
-
-enum
-{
-    max_length = 1024
-};
 
 int main(int argc, char *argv[])
 {
@@ -39,45 +24,34 @@ int main(int argc, char *argv[])
         tcp::resolver resolver(io_service);
         boost::asio::connect(s, resolver.resolve({"127.0.0.1", "1234"}));
 
-        std::cout << "Enter message: ";
-        char request[max_length];
-        std::cin.getline(request, max_length);
-        size_t request_length = std::strlen(request);
-
         Customer customer(1001, "Tartempion1",
-                       {10000, 10001});
-
+                          {10000, 10001});
+        // Customer customer;
         boost::asio::streambuf buf;
-
-        // counter_streambuf buf;
-
         std::ostream os(&buf);
-        // boost::archive::text_oarchive ar(os);
-        boost::archive::binary_oarchive ar(os, boost::archive::no_header);
+        boost::archive::text_oarchive ar(os);
         ar << customer;
 
         std::cout << "The number of bytes taken for " << buf.size() << std::endl;
+        auto data = to_string(buf);
 
-        boost::system::error_code error;
-        boost::asio::write(s, buf);
-        // boost::asio::write(s, boost::asio::buffer(buf, 10000));
-        if (!error)
-        {
-            std::cout << "Client sent !" << std::endl;
-        }
-        else
-        {
-            std::cout << "Client sent failed: " << error.message() << std::endl;
-        }
+               std::cout << "Data : " << data.c_str() <<" "<< strlen(data.c_str())<< std::endl;
 
-        // boost::asio::write(s, boost::asio::buffer(request, request_length));
+        std::cout << customer << std::endl;
+        boost::asio::write(s, boost::asio::buffer(to_string(buf).c_str(), buf.size()));
+
+
+
 
         char reply[max_length];
-        size_t reply_length = boost::asio::read(s,
-                                                boost::asio::buffer(reply, request_length));
-        std::cout << "Reply is: ";
-        std::cout.write(reply, reply_length);
-        std::cout << "\n";
+        boost::system::error_code error;
+        // size_t reply_length = boost::asio::read(s,
+        //                                         boost::asio::buffer(reply, buf1.size()));
+        size_t length = s.read_some(boost::asio::buffer(reply), error);
+        std::cout << "Data : " << reply <<" "<< strlen(reply)<< std::endl;
+        // std::cout << "Reply is: ";
+        // std::cout.write(reply, reply_length);
+        // std::cout << "\n";
     }
     catch (std::exception &e)
     {
---
+++ Project/socket/test_socket/socket_tools.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <boost/asio.hpp>
+
+std::string to_string(boost::asio::streambuf &buffer);
